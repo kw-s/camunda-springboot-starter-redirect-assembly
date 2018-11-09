c8ca70ee7e6afb2d66ab1d713e2d58798e3c8fdf
Weissmann.Kristian <weissmann.kristian@swm.de>
2018-11-09 10:43:55 +0100

    initial import

---
+++ assembly/src/main/java/foo/bar/camunda/springboot/starter/redirectproblem/assemblybased/ApplicationAssemblyBased.java
@@ -0,0 +1,14 @@
+package foo.bar.camunda.springboot.starter.redirectproblem.assemblybased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ApplicationAssemblyBased {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationAssemblyBased.class, args);
+	}
+}
